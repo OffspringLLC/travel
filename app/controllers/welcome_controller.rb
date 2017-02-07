c5a666554d81ed69b564d4a68ec6f72c796649e0
OffspringLLC <aarmour@offspringsolutions.com>
2017-02-06 19:58:34 -0500

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,12 @@
+class WelcomeController < ApplicationController
+  def index
+  	@homeland = 'Puerto Rico'
+  	@countries = ['Chile','Bolivia','Croatia']
+  	@travel_pics = ['chile.jpg','bolivia.jpg','croatia.jpg']
+  end
+
+  def about
+  	@color = params[:color]
+  	@size = params[:size].to_i
+  end
+end
